9d15583e0f0e7d709c80570d67e02f93271e3941
Patience Dendy <dendypat@gmail.com>
2020-12-05 10:54:49 -0500

    Mongoose Population

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,33 +1,34 @@
 const express = require('express');
 const Partner = require('../models/partner');
+const authenticate = require('../authenticate');
 
 const partnerRouter = express.Router();
 
 partnerRouter.route('/')
 .get((req, res, next) => {
   Partner.find()
-    .then(promotions => {
+    .then(partners => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application');
-      res.json(promotions);
+      res.json(partners);
     })
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
   Partner.create(req.body)
-    .then(promotion => {
-      console.log('Promotion Created', promotion);
+    .then(partner => {
+      console.log('Promotion Created', partner);
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application');
-      res.json(promotion);
+      res.json(partner);
     })
     .catch(err => next(err));
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
   res.statusCode = 403;
   res.end('PUT operation not supported on /partner');
 })
-.delete((reg, res) => {
+.delete(authenticate.verifyUser, (reg, res, next) => {
   Partner.deleteMany()
     .then(response => {
       res.statusCode = 200;
@@ -40,19 +41,19 @@ partnerRouter.route('/')
 
 partnerRouter.route('/:partnerId')
 .get((req, res, next) => {
-  Campsite.findById(req.params.campsiteId)
-      .then(campsite => {
+  Partner.findById(req.params.partnerId)
+      .then(partner => {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/json');
-          res.json(campsite);
+          res.json(partner);
       })
       .catch(err => next(err));
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
   res.statusCode = 403;
-  res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+  res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
   Partner.findByIdAndUpdate(req.params.partnerId, {
       $set: req.body
   }, { new: true })
@@ -63,7 +64,7 @@ partnerRouter.route('/:partnerId')
       })
       .catch(err => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
   Partner.findByIdAndDelete(req.params.partnerId)
       .then(response => {
           res.statusCode = 200;
